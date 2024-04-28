9e109009fc9dab2ff692d3c9d5f9ec1a86d5e20f
Andres Court <andres@andrescourt.com>
2024-04-27 22:09:35 -0500

    Installed Tanstack React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
+import { useQuery } from "@tanstack/react-query";
+import { getFoo } from "./queries/foo";
+import Loading from "./components/Loading";
+import Error from "./components/Error";
+
 function App() {
-  return (
-      <div className="text-3xl font-bold underline">Hello world!</div>
-  )
+    const {
+        data: foo,
+        isLoading,
+        error,
+        isError,
+    } = useQuery({
+        queryKey: ["foo"],
+        queryFn: getFoo,
+    });
+
+    if (isLoading) {
+        return <Loading />;
+    }
+
+    if (isError) {
+        return <Error message={error.message} />;
+    }
+
+    return (
+        <>
+            <div className="text-3xl font-bold underline">Hello world!</div>
+            <div>{foo?.message}</div>
+        </>
+    );
 }
 
-export default App
+export default App;
